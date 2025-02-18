3e8d84807b8c37f7774f458ee062ddc4af568093
Laura Ferreira de Almeida <laura.ferreira@ufba.br>
2025-02-17 21:00:08 -0300

    pré-processamento e medidas do dataset

--- main.py
+++ main.py
@@ -22,3 +22,46 @@ wine_df = pd.read_csv('/content/drive/MyDrive/Colab Notebooks/Analysis.csv')
 
 atributos = wine_df.columns
 print(atributos)
+
+# Resumo estístico básico
+wine_df.describe()
+
+# removendo a coluna 'Unnamed: 0'
+wine_df.drop(columns=['Unnamed: 0'], inplace=True)
+wine_df.head(5)
+
+# checar os valores nulos
+wine_df.isnull().sum()
+
+
+# medidas de tendência central
+dados_numericos = wine_df.select_dtypes(include=[float,int])
+
+
+for coluna in dados_numericos.columns:
+    print(f"Média da coluna {coluna}: {dados_numericos[coluna].mean()}")
+    print(f"Mediana da coluna {coluna}: {dados_numericos[coluna].median()}")
+    print(f"Moda da coluna {coluna}: {dados_numericos[coluna].mode()[0]}")
+    print()
+
+
+# medidas de tendência central
+dados_numericos = wine_df.select_dtypes(include=[float,int])
+
+
+for coluna in dados_numericos.columns:
+    print(f"Média da coluna {coluna}: {dados_numericos[coluna].mean()}")
+    print(f"Mediana da coluna {coluna}: {dados_numericos[coluna].median()}")
+    print(f"Moda da coluna {coluna}: {dados_numericos[coluna].mode()[0]}")
+    print()
+
+
+# medidas de tendência central
+dados_numericos = wine_df.select_dtypes(include=[float,int])
+
+
+for coluna in dados_numericos.columns:
+    print(f"Média da coluna {coluna}: {dados_numericos[coluna].mean()}")
+    print(f"Mediana da coluna {coluna}: {dados_numericos[coluna].median()}")
+    print(f"Moda da coluna {coluna}: {dados_numericos[coluna].mode()[0]}")
+    print()
